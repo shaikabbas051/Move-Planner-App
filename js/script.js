c2716603c02c334fa9edef94c6596b9933e1ab4f
shaik abbas <shaikabbas000@gmail.com>
2017-09-10 12:41:10 +0530

    added street view
    using jQuery method street view image is added to the body

--- js/script.js
+++ js/script.js
@@ -12,11 +12,11 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    // YOUR CODE GOES HERE!
     var street = $("#street").val();
     var city = $("#city").val();
     var address = street +','+ city;
     $body.append('<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location= '+address+'">')
-    // YOUR CODE GOES HERE!
 
     return false;
 };
